c658d9440b6962ff650c70bb81f844671e5d71d1
Lut4Sayem <lutfursayem09@gmail.com>
2025-03-25 02:08:53 +0600

    Weekly CP upload

---
+++ test3.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"just trying things";
+    
+    return 0;
+}
---
+++ upload.sh
@@ -0,0 +1,1 @@
+git add . && git commit -m 'Weekly CP upload' && git push origin main
