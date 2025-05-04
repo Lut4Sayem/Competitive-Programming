1a65d506ceeb98f330d542f3f3407aa6ba964886
Lut4Sayem <lutfursayem09@gmail.com>
2025-05-04 23:54:28 +0600

    Weekly CP upload - New files added

---
+++ week_6/A_Special_Characters.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define main() ashen_shuru_kori
+int ashen_shuru_kori(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    return 0;
+}
